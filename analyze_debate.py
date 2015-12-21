3ead44a89f062722b883698aaf259ef73be7e2e4
Kevin Kirchhoff <kkirchhoff01@yahoo.com>
2015-12-21 17:57:18 -0600

    require candidate command line option to speed up parsing

--- analyze_debate.py
+++ analyze_debate.py
@@ -7,12 +7,6 @@ from nltk.corpus import stopwords
 
 candidate_key = ''
 
-if len(sys.argv) < 2:
-    print('No transcript provided')
-    sys.exit()
-elif len(sys.argv) == 3:
-    candidate_key = sys.argv[2].lower()
-
 # December 15 debate candidates and moderators
 
 CANDIDATES = {
@@ -33,6 +27,15 @@ MODERATORS = {
             'bash' : []
             }
 
+if len(sys.argv) != 3:
+    print("Not enough arguments provided\nUse python analyze_debate.py <transcript> <candidate>")
+    sys.exit()
+else:
+    candidate_key = sys.argv[2].lower()
+    if candidate_key not in CANDIDATES.keys():
+        print("Candidate not found.")
+        sys.exit()
+
 topics = []
 
 try:
@@ -48,6 +51,7 @@ def parse(data):
 
 current_speaker = ''
 for line in fh:
+    # nltk has some problems with ascii encoding in python 2.7.*
     try:
         line = line.encode('ascii','ignore')
     except UnicodeError:
@@ -59,24 +63,30 @@ for line in fh:
                 pass
         line = templine
     data = line.split(':')
+
+    # Empty line
     if len(data) < 1:
         pass
+
+    # New comment
     elif len(data) == 2:
         current_speaker = data[0].lower()
-        if current_speaker in MODERATORS.keys():
+        if current_speaker in MODERATORS.keys() and current_speaker == candidate_key:
             topics += nltk.pos_tag(nltk.word_tokenize(data[1]))
 
-        if current_speaker in CANDIDATES.keys():
+        if current_speaker in CANDIDATES.keys() and current_speaker == candidate_key:
             if data[1] != '\n':
                 CANDIDATES[current_speaker] = CANDIDATES[current_speaker] + parse(nltk.word_tokenize(data[1]))
-        elif current_speaker in MODERATORS.keys():
+        elif current_speaker in MODERATORS.keys() and current_speaker == candidate_key:
             if data[1] != '\n':
                 MODERATORS[current_speaker] = MODERATORS[current_speaker] + parse(nltk.word_tokenize(data[1]))
+
+    # Continued comment
     elif len(data) == 1 and line != '':
-        if current_speaker in CANDIDATES.keys():
+        if current_speaker in CANDIDATES.keys() and current_speaker == candidate_key:
             if data[0] != '\n':
                 CANDIDATES[current_speaker] = CANDIDATES[current_speaker] + parse(nltk.word_tokenize(data[0]))
-        elif current_speaker in MODERATORS.keys():
+        elif current_speaker in MODERATORS.keys() and current_speaker == candidate_key:
             if data[0] != '\n':
                 MODERATORS[current_speaker] = MODERATORS[current_speaker] + parse(nltk.word_tokenize(data[0]))
     else:
@@ -86,6 +96,5 @@ fh.close()
 
 # Print sorted words
 
-if candidate_key in CANDIDATES.keys():
-    fdist = nltk.FreqDist(CANDIDATES[candidate_key])
-    fdist.plot(25, cumulative=False, title=candidate_key)
+fdist = nltk.FreqDist(CANDIDATES[candidate_key])
+fdist.plot(25, cumulative=False, title=candidate_key)
